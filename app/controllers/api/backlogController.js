aa2c7535bd4c885c48d7faeb5364ea6d7d191342
shanmuga <sundaraswamy@gmail.com>
2021-06-14 15:00:42 +0530

    updated code

---
+++ app/controllers/api/backlogController.js
@@ -0,0 +1,116 @@
+let express = require('express');
+let router = express.Router();
+let asyncHandler = require('../../utils/asyncHandler');
+let backLogService = require('../../services/backlogService');
+let {authJwt} = require('../../middlewares/authMiddleware');
+
+router.get(
+    '/find/:id',
+    authJwt(),
+    asyncHandler(async (req, res) => {
+        const result = await epicService.getOne(req);
+
+        if (result.length === 0) { res.send({success:false, message:res.__('api.client.get.error')}); }
+        else { res.send({success: true, data: result}); }
+    })
+);
+
+router.get(
+    '/all',
+    authJwt(),
+    asyncHandler(async (req, res) => {
+        const page = req.body.page || req.query.page || 1;
+        const pageSize = req.body.pageSize || req.query.pageSize || 10;
+        const result = await backLogService.getAll(req, page, pageSize);
+
+        if (result.length === 0) {
+            res.send({success:false, message:res.__('api.client.get.empty')});
+        } else {
+            let total = await backLogService.getAllCount();
+            res.send({success: true, data: result, total: total, pageSize, page: page});
+        }
+    })
+);
+
+
+router.get(
+    '/dept/all',
+    // authJwt(),
+    asyncHandler(async (req, res) => {
+        const result = await epicService.getDept(req);
+
+        if (result.length === 0) { res.send({success:false, message:res.__('api.epic.get.error')}); }
+        else { res.send({success: true, data: result}); }
+    })
+);
+
+router.get(
+    '/dept/epic',
+    authJwt(),
+    asyncHandler(async (req, res) => {
+        const result = await epicService.getDeptWorkorder(req.query.dept);
+
+        if (result.length === 0) { res.send({success:false, message:res.__('api.epic.get.error')}); }
+        else { res.send({success: true, data: result}); }
+    })
+);
+
+router.get(
+    '/workorder/all',
+    authJwt(),
+    asyncHandler(async (req, res) => {
+        const result = await epicService.getWorkOrders(req);
+
+        if (result.length === 0) { res.send({success:false, message:res.__('api.epic.get.error')}); }
+        else { res.send({success: true, data: result}); }
+    })
+);
+router.post(
+    '/save',
+    authJwt(),
+    asyncHandler(async (req, res) => {
+        req.checkBody('dept_id').trim().notEmpty();
+        req.checkBody('workOrder_desc').trim().notEmpty();
+
+        let errors = req.validationErrors();
+
+        if (errors) {
+            res.send({
+                success: false,
+                message: res.__('api.client.fields.empty')
+            });
+        } else {
+            const id = req.body.id;
+            console.log("body " + id);
+            const result = await epicService.save(req, id);
+
+            if (result) {
+                res.send({
+                    success: true,
+                    message: res.__((id ? 'api.client.update.success' : 'api.client.save.success'))
+                });
+            } else {
+                res.send({
+                    success: false,
+                    message: res.__((id ? 'api.client.update.error' : 'api.client.save.error'))
+                });
+            }
+        }
+    })
+);
+
+router.delete(
+    '/delete/:id',
+    authJwt(),
+    asyncHandler(async (req, res, next) => {
+        const result = await epicService.delete(req);
+
+        if (result!==null) {
+            res.send({success: true, message: res.__('api.client.delete.succes')});
+        } else {
+            res.send({success: false, message: res.__('api.client.delete.error')});
+        }
+    })
+);
+
+module.exports = router;
--- app/services/backlogService.js
+++ app/services/backlogService.js
@@ -1,7 +1,7 @@
 let db = require('../../db/dataBase');
 const oracledb = require("oracledb");
 let DropDown = require('../vo/dropdown');
-let EpicValueObject = require('../vo/epicValueObject');
+let BackLogValueObject = require('../vo/backlogVo');
 class epicService {
 
     static async delete(req) {
@@ -21,7 +21,7 @@ class epicService {
         const result = await db.connection.execute("select dept_id, epic_id,epic_desc,user_story_id, user_story_task  from sci_standard_epic_Data where user_Story_id = :user_story_id",[id]);
 
         result.rows.forEach((row) => {
-            resultArray.push(new EpicValueObject(row[0],row[1],row[2],row[3],row[4]));
+            resultArray.push(new BackLogValueObject(row[0],row[1],row[2],row[3],row[4]));
         });
         return resultArray;
     }
@@ -63,22 +63,29 @@ class epicService {
     static async getAll(req, page, pageSize) {
         let active = req.query.active || 'id';
         const order = req.query.order || 'desc';
-        let search = req.body.search || req.query.search;
+        let searchByDept = req.body.searchByDept || req.query.searchByDept;
+        let searchByDesc = req.body.searchByDesc || req.query.searchByDesc;
+        let searchByWork = req.body.searchByWork || req.query.searchByWork;
         const newPage = (page -1) * pageSize;
-        let searchParam = '%'+search+'%';
-        if (search === undefined) {search = '%%';}
+        let searchByDeptParam = '%'+searchByDept+'%';
+        if (searchByDeptParam === undefined) {searchByDeptParam = '%%';}
+
+        let searchByDescParam = '%'+searchByDesc+'%';
+        if (searchByDescParam === undefined) {searchByDescParam = '%%';}
+        let searchByWorkParam = '%'+searchByWork+'%';
+        if (searchByWorkParam === undefined) {searchByWorkParam = '%%';}
         let resultArray = new Array();
 
-        const result = await db.connection.execute("select ab.dept_id,ab.epic_id ,ab.epic_desc, ab.user_story_id,ab.user_story_task from  (select rownum as rn ,dept_id,epic_id ,epic_desc, user_story_id,user_story_task from sci_Standard_epic_Data  where (dept_id like :search1 or user_story_id like :search2 or epic_desc like :search3)  ) ab where ab.rn between :startlimit and :endlimit ",[searchParam,searchParam,searchParam,newPage,parseInt(newPage) +parseInt(pageSize)]
+        const result = await db.connection.execute("select ab.dept_id ,ab.epic_desc, ab.user_story_id,ab.user_story_task ,ab.epic_status ,ab.workorder_ref,ab.seq_work_id from  (select rownum as rn ,dept_id,epic_desc, user_story_id,user_story_task,epic_status ,workorder_ref,seq_work_id from SCI_BACKLOG_MASTER  where dept_id like :search1 and user_story_task like :search2 and seq_work_id like :search3 ) ab where ab.rn between :startlimit and :endlimit ",[searchByDeptParam,searchByDescParam,searchByWorkParam,newPage,parseInt(newPage) +parseInt(pageSize)]
         );
         result.rows.forEach((row) => {
-            resultArray.push(new EpicValueObject(row[0],row[1],row[2],row[3],row[4]));
+            resultArray.push(new BackLogValueObject(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7]));
         });
         return resultArray;
     }
 
     static async getAllCount() {
-        let result = await db.connection.execute("select count(1) from sci_Standard_epic_Data");
+        let result = await db.connection.execute("select count(1) from sci_backlog_master");
         let total = result.rows[0][0];
         return total;
     }
@@ -86,16 +93,15 @@ class epicService {
     static async save(req, id) {
 
         console.log(req.user);
-      let dept_id = req.body.dept_id;
-      let workOrderDesc = req.body.workOrder_desc;
+        let dept_id = req.body.dept_id;
+        let workOrderDesc = req.body.workOrder_desc;
 
-      let standardEpicId = req.body.standard_epic_id;
+        let standardEpicId = req.body.standard_epic_id;
 
         standardEpicId.forEach(row => {
             db.connection.execute("delete from sci_backlog_master where DEPT_ID=:dept_id and user_story_id =:story_id",[dept_id,row],{autoCommit:true});
-            db.connection.execute("    INSERT INTO \"SCIGENICS\".\"SCI_BACKLOG_MASTER\" (          seq_backlog_id,          dept_id,          user_story_task,          epic_desc,          user_story_id,          created_date,          created_by,          epic_status,          updated_by,          updated_date,          workorder_ref     ,seq_work_id ) VALUES (          sci_backlog_master_seq.NEXTVAL,          :dept_id,          (select epic_desc||'-'|| stage_desc|| '-' || user_story_task  from SCI_STANDARD_EPIC_DATA where user_Story_id = :user_Story_id),        (select epic_desc from SCI_STANDARD_EPIC_DATA where user_Story_id = :user_Story_id),          :user_story_id,          sysdate,         :logged_in_user,         'BKLOG',           :logged_in_user,        sysdate,          :work_order_ref,          (select seq_work_id from SCI_WORKORDER_MASTER where job_desc = :work_order_ref)      )",
+            db.connection.execute("    INSERT INTO \"SCIGENICS\".\"SCI_BACKLOG_MASTER\" (          seq_backlog_id,          dept_id,          user_story_task_desc,          epic_desc,          user_story_id,          created_date,          created_by,          epic_status,          updated_by,          updated_date,          workorder_ref     ,seq_work_id ) VALUES (          sci_backlog_master_seq.NEXTVAL,          :dept_id,          (select epic_desc||'-'|| stage_desc|| '-' || user_story_task  from SCI_STANDARD_EPIC_DATA where user_Story_id = :user_Story_id),        (select epic_desc from SCI_STANDARD_EPIC_DATA where user_Story_id = :user_Story_id),          :user_story_id,          sysdate,         :logged_in_user,         'BKLOG',           :logged_in_user,        sysdate,          :work_order_ref,          (select seq_work_id from SCI_WORKORDER_MASTER where job_desc = :work_order_ref)      )",
                 [dept_id,row,row,row,req.user,req.user,workOrderDesc,workOrderDesc] ,{ autoCommit: true });
-
         });
 
         return true;
